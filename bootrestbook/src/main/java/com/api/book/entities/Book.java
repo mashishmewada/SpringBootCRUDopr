b395a8d77d0ff35412827937f5b1776985fefdb1
Ashish <Ashish@192.168.8.72>
2021-12-16 19:30:00 +0530

    Book Entity file

--- bootrestbook/src/main/java/com/api/book/entities/Book.java
+++ bootrestbook/src/main/java/com/api/book/entities/Book.java
@@ -1,59 +1,59 @@
 package com.api.book.entities;
 
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "books")
+@Getter
+@Setter
+@ToString
 public class Book {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "book_id")
 	private int id;
+	
+	private String bookName;
+	
 	private String title;
-	private String author;
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getAuthor() {
-		return author;
-	}
-	public void setAuthor(String author) {
-		this.author = author;
-	}
 	
-	public Book(int id, String title, String author) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.author = author;
-	}
+	@OneToOne(cascade = CascadeType.ALL)
+	private Author author;
 	
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private Set<Page> pages;
+
+
 	public Book() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	@Override
-	public String toString() {
-		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+
+
+	public Book(int id, String bookName, String title, Author author, Set<Page> pages) {
+		super();
+		this.id = id;
+		this.bookName = bookName;
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
 	}
 	
 	
-
 }
