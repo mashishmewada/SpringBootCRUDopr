995dea2e0262f47c0b54e1b8476e7ed4c63aa163
Ashish <Ashish@DESKTOP-3UTF11M>
2021-12-16 23:49:18 +0530

    Author Entity file

--- bootrestbook/src/main/java/com/api/book/entities/Author.java
+++ bootrestbook/src/main/java/com/api/book/entities/Author.java
@@ -1,14 +1,18 @@
 package com.api.book.entities;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class Author {
 	
 	@Id
@@ -17,14 +21,6 @@ public class Author {
 	private String firstName;
 	private String lastName;
 	private String language;
-	
-	//@JsonBackReference
-	//@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	//@JoinColumn(name = "book_id")
-    @JoinColumn(name = "author_book_id", referencedColumnName = "book_id")
-    @ManyToOne(cascade = CascadeType.ALL)
-	private Book book;
-	
 	
 	public Author() {
 		super();
@@ -39,35 +35,5 @@ public class Author {
 		this.language = language;
 	}
 	
-	public int getAuthorId() {
-		return authorId;
-	}
-	public void setAuthorId(int authorId) {
-		this.authorId = authorId;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getLanguage() {
-		return language;
-	}
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-	
-	@Override
-	public String toString() {
-		return "Author [authorId=" + authorId + ", firstName=" + firstName + ", lastName=" + lastName + ", language="
-				+ language + "]";
-	}
 	
 }
